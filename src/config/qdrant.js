6fd7af77bdffecd37baa5d971e925c622be5d957
Kartheeswaran0512 <88021315+Kartheeswaran0512@users.noreply.github.com>
2026-07-19 13:35:12 +0530

    Clarify checkCompatibility option in Qdrant client
    Added comment to clarify the purpose of the checkCompatibility option.

--- src/config/qdrant.js
+++ src/config/qdrant.js
@@ -1,10 +1,11 @@
 const { QdrantClient } = require('@qdrant/js-client-rest');
 require('dotenv').config();
 
+// Connects to our Qdrant Cloud vector database
 const client = new QdrantClient({
   url: process.env.QDRANT_URL,
   apiKey: process.env.QDRANT_API_KEY,
-  checkCompatibility:false,
+  checkCompatibility: false, // skips version-check that caused earlier errors
 });
 
 module.exports = client;
